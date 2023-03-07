a4ce516f4475eb69e1281ee9eeb8f910c10ec58e
adanhm22 <43605705+adanhm22@users.noreply.github.com>
2023-03-07 14:10:24 +0100

    hechos ej funciones, falta arreglar el ultimo

--- funciones.php
+++ funciones.php
@@ -4,7 +4,7 @@
  */
 function salto(){echo "<br><br>";}
 echo "ej 1:<br>";
-function numeroMenor ($num1,$num2,$num3){
+function ej1 ($num1,$num2,$num3){
     if ($num1 < $num2){
         if($num1<$num3){
             return $num1;
@@ -21,14 +21,14 @@ function numeroMenor ($num1,$num2,$num3){
 }
 $num1 = 4;$num2 = 7;$num3 = 5; 
 
-$menor = numeroMenor($num1,$num2,$num3);
+$menor = ej1($num1,$num2,$num3);
 echo "el menor de los 3 es $menor";
 salto();
 /**
  * Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.
  */
 echo "ej 2;<br>";
-function contador ($frase,$letra){
+function ej2 ($frase,$letra){
     $i = 0;
     $numero = 0;
     while ($i < strlen($frase)){
@@ -38,7 +38,7 @@ function contador ($frase,$letra){
     return $numero;
 }
 $frase = "hola muy buenas a todos";$letra = 'a';
-$numeroLetras = contador($frase,$letra);
+$numeroLetras = ej2($frase,$letra);
 echo "en la frase \"$frase\" hay un total de $numeroLetras $letra"."es";
 salto();
 
@@ -46,14 +46,14 @@ salto();
  * Suma o resta (según elija el usuario) dos números reales
  */
 "echo ej3: <br>";
-function operacion ($num1,$num2, $tipo){
+function ej3 ($num1,$num2, $tipo){
     if ($tipo == "resta") return $num1 - $num2;
     if ($tipo == "suma") return $num1 + $num2;
     return -1;
 }
 $numero1 = 12;$numero2 = 24;
 $tipoOperacion = "resta";
-$resultado = operacion ($numero1,$numero2,$tipoOperacion);
+$resultado = ej3 ($numero1,$numero2,$tipoOperacion);
 echo "la $tipoOperacion de $numero1 y $numero2 es $resultado";
 salto();
 
@@ -61,4 +61,90 @@ salto();
  * Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide (dispone de 3 intentos)
  */
 echo "ej 4: <br>";
+$usuario = "Adan";$contrasennia = "prueba";
+$usuarios = ["Adan"=> "prueba"];
+$intentos = 3;
+
+function ej4($usuario,$contrasennia)
+{
+    //esto hace paso por referencia como pensaba
+    global $usuarios, $intentos;
+    if ($intentos <= 0) return "no hay más intentos";
+    if (!array_key_exists($usuario,$usuarios)) {
+        $intentos --;
+        return "no existe el usuario, número de intentos $intentos";
+    }else{
+        if ($usuarios[$usuario]==$contrasennia){
+            return "login hecho";
+        }else{
+            $intentos --;
+            return "error en el login, número de intentos $intentos";
+        }
+    }
+}
+$respuesta = ej4($usuario,$contrasennia);
+echo $respuesta . "<br>";
+$respuesta = ej4($usuario,$contrasennia);
+echo $respuesta . "<br>";
+salto();
+
+/**
+ * Solicita al usuario una letra, si inserta una a muestra el número 7,
+ * si es una b, el 9, si es una c el 101 y si no es ninguno de los tres, indícale que se ha equivocado de letra
+ */
+    echo "ej 5: <br>";
+    $letra = 'l';
+    function ej5($letra){
+        if ($letra == 'a'){
+            return 7;
+        } elseif ($letra == 'b'){
+            return 9;
+        }elseif ($letra == 'c'){
+            return 101;
+        }else{
+            return "error en la letra";
+        }
+    }
+
+    echo "el programa ha devuelto: " . ej5($letra);
+    salto();
+
+    /**
+     * Ordena alfabéticamente un array con 7 palabras puedes usar el algoritmo de la burbuja
+     */
+    echo "ej 6 :<br>";
+    $palabras = ["algo","todo","nada","mucho","poco","bastante","entero"];
+
+    function ej6($palabras)
+    {
+        $longitud = count ($palabras);
+        $completado = false;
+        $contador = 0;
+        $x = 0; $y = 1;
+        while (!$completado){
+            if (strnatcmp($palabras[$x],$palabras[$y]) > 0 ){
+
+                $temporal = $palabras[$x];
+                $palabras[$x] = $palabras[$y];
+                $palabras[$y] = $temporal;
+                $x++; $y++;
+                $contador = 0;
+            }else{
+                $x++; $y++;
+                $contador++;
+            }
+            if ($y >= $longitud){
+                $x = 0; $y = 1;
+                if ($contador +1 >= $longitud) $completado = true;
+                $contador = 0;
+            }
+        }
+        return $palabras;
+    }
+
+    var_dump(ej6($palabras));
+    salto();
+
+
+
 ?>
---
+++ funciones2.php
@@ -0,0 +1,99 @@
+<?php
+    function salto(){echo "<br><br>";}
+    /**
+     * Una función que devuelva el número de cifras de un entero pasado como parámetro.
+     */
+    echo "ej 1: <br>";
+    function ej1($numero)
+    {
+        $cifras = 0; $numero2= 1;
+        do {
+            $numero2 = $numero2 * 10;
+            $cifras++;
+        }while($numero>$numero2);
+        return $cifras;
+    }
+    echo ej1(456);
+    salto();
+
+    /**
+     *  Una función que muestre al usuario una secuencia de *
+     * (se debe construir la cadena de uno en uno), la cantidad de * se pasará cómo parámetro de la función.
+     */
+    echo "ej2: <br>";
+    function ej2($numero)
+    {
+        $resultado = "";
+        for ($i=0; $i < $numero; $i++) { 
+            $resultado = $resultado . "*";
+        }
+        return $resultado;
+    }
+    echo ej2(22);
+    salto();
+    /**
+     * Una función que permita mostrar la secuencia (se debe construir la cadena de uno en uno):
+     */
+    echo "ej3: <br>";
+    function ej3($numero)
+    {
+        $resultado = "";
+        for ($i=0; $i < $numero; $i++) { 
+            $resultado = $resultado . "*+_";
+        }
+        return $resultado;
+    }
+    echo ej3(12);
+    salto();
+
+    /**
+     * Una función que permita mostrar un triángulo como el siguiente:
+     */
+    echo "ej4: <br>";
+    function ej4($numero)
+    {
+        $resultado = "";
+        for ($i=0; $i < $numero; $i++) { 
+            for ($y=0; $y < $i; $y++) { 
+                $resultado = $resultado . "*";
+            }
+            $resultado = $resultado . "<br>";
+        }
+        return $resultado;
+    }
+    echo ej4(5);
+    salto();
+
+    /**
+     * Una función que devuelva la diferencia en días entre dos fechas del mismo año (sólo tenemos en cuenta dia y mes).
+     */
+    echo "ej5:<br>";
+    function sumaMes ($mes){
+        $meses31 = [1,3,5,7,8,10,12];
+        if (in_Array($mes,$meses31)){
+           return 31;
+        }elseif ($mes == 2){
+            return 28;
+        }else{
+            return 30;
+        }
+    }
+    function ej5($dia,$mes,$dia2,$mes2)
+    {
+        
+        $diasEntre = 0;
+        $mesesEntre = $mes2 - $mes;
+        for ($i=0; $i < $mesesEntre; $i++) { 
+            $diasEntre += sumaMes($mes);
+            $mes ++;
+        }
+        if ($dia < $dia2){
+            $diasEntre += sumaMes($mes2 -1) - $dia2;
+        }else{
+            $diasEntre += $dia2 - $dia;
+        }
+
+        return $diasEntre;
+    }
+    echo ej5(2,3,15,5);
+?>
