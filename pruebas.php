fe4a239a40f5df67539c7b9a750e2b77d79d31fc
adanhm22 <43605705+adanhm22@users.noreply.github.com>
2023-03-14 14:02:56 +0100

    pruebas abstracto

--- pruebas.php
+++ pruebas.php
@@ -12,8 +12,15 @@
         
     }
 
+    function correr (Persona $persona, int $bucle)
+    {
+        for ($i=0; $i < $bucle; $i++) { 
+            $persona->andar();
+        }
+    }
+
     $alumno = new class extends Persona{
-        public function andar (){echo __METHOD__;}
+        public function andar (){echo __METHOD__ . "<br>";}
     };
     var_dump($alumno);
     $alumno->andar();
@@ -21,4 +28,8 @@
     $algo = fn($valor) => $valor * $valor;
 
     echo "<br> " . $algo(7);
+
+    echo "<br>";
+    correr($alumno,4);
+
 ?>
